09bf55c50252939c56c205039d36af039cc61a66
Santiago Guzmán Jaimes <santi03312005@gmail.com>
2023-04-01 22:28:45 -0500

    Mobile menu added

--- archivo.js
+++ archivo.js
@@ -1,1 +1,9 @@
-console.log("Esto es una prueba");
+const menuHamIcon = document.querySelector('.menu_burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactivo');
+}
